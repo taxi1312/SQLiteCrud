a3157a6b04e7032affaa4f9372f10ef9a09c1036
taxi4312 <taxi4312kn@gmail.com>
2018-03-23 12:25:18 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ongor1/sqlitecrud/Content_main2.java
@@ -0,0 +1,13 @@
+package com.example.ongor1.sqlitecrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Content_main2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main2);
+    }
+}
---
+++ app/src/main/java/com/example/ongor1/sqlitecrud/item_layout.java
@@ -0,0 +1,13 @@
+package com.example.ongor1.sqlitecrud;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class item_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_layout);
+    }
+}
